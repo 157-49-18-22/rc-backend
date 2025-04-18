222127e6697b62c86b45e355f37d1c1c20c82dce
luvsharmag <luvsharmgju31@gmail.com>
2025-04-18 21:26:29 +0530

    exex

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
-const balanceRoutes = require('./routes/balanceRoutes');
-const cors = require('cors');
+const balanceRoutes = require("./routes/balanceRoutes");
+const cors = require("cors");
 dotenv.config();
 connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.use('/api/users', userRoutes);
-app.use('/api/balance', balanceRoutes);
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Backend is running fine!" });
+});
+app.use("/api/users", userRoutes);
+app.use("/api/balance", balanceRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
